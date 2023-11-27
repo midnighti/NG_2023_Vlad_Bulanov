6e5f4f8f4e453b56026b2334465636098461ab9a
midnighti <midnighti@EOM.Eom>
2023-11-27 10:57:43 +0200

    simplified task 4

--- Lesson1/Task 4/main.py
+++ Lesson1/Task 4/main.py
@@ -10,45 +10,20 @@ def userInputCalcMode():
     mode = input("choose aperation: +, -, *, /, sqrt, degree - ")
     return mode
 
-
-def add(valueA, valueB):
-    return print(valueA + valueB)
-
-def subtract(valueA, valueB):
-    return print(valueA - valueB)
-
-def multiply(valueA, valueB):
-    return print(valueA * valueB)
-
-def devide(valueA, valueB):
-    return print(valueA / valueB)
-
-def sqrt(valueA, valueB):
-    return print(gmpy2.root(valueA, valueB))
-
-def degree(valueA, valueB):
-    return print(pow(valueA, valueB))
-
-
 def operation(valueA, valueB, mode):
     match mode:
         case "+":
-            return add(valueA, valueB)
+            return print(valueA + valueB)
         case "-":
-            return subtract(valueA, valueB)
+            return print(valueA - valueB)
         case "*":
-            return multiply(valueA, valueB)
+            return print(valueA * valueB)
         case "/":
-            return devide(valueA, valueB)
+            return print(valueA / valueB)
         case "sqrt":
-            return sqrt(valueA, valueB)
+            return print(gmpy2.root(valueA, valueB))
         case "degree":
-            return degree(valueA, valueB)
+            return print(pow(valueA, valueB))
         
+operation(userInputNum(), userInputNum(), userInputCalcMode())
 
-def main():
-    operation(userInputNum(), userInputNum(), userInputCalcMode())
-
-
-if __name__ == "__main__":
-    main()
